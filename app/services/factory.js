6e607832bdf62c3d1cc77cce2abfbee833bd9bd7
Brendan Martin <bmartin@zonoff.com>
2016-10-30 13:13:31 -0400

    Moved factory to an upgradeAdapter service. Doesnt run

--- app/services/factory.js
+++ app/services/factory.js
@@ -3,24 +3,45 @@
  * New hotness
  */
 // use uA to add providers
-// upgradeAdapter.addProvider(ng.http.HTTP_PROVIDERS);
+var upgradeAdapter = new ng.upgrade.UpgradeAdapter();
+upgradeAdapter.addProvider(ng.http.HTTP_PROVIDERS);
 
-// var factoryService
+console.log(ng.http.Http)
+
+
+var factoryService = ng.core.
+  Injectable().
+  Class({
+    // fake $http injection
+    constructor: [ng.http.Http, function(http) {
+      // this.http = http;
+    }],
+    getNews: function() {
+      // return this.http.get('http://weather.com').map(
+      //   function(res) {
+      //     return res.json();
+      //   }
+      // ).toPromise();
+    }
+  });
+
+angular.module('myApp.factoryService', [])
+.factory('factoryService', factoryService)
 
 
 /*
- * Busted ass old code
+ * Old busted ass
  */
-angular.module('myApp.factoryService', [])
-.factory('factoryService', ['$http', function($http){
-  return {
-    getNews: function() {
-      return $http.get('http://weather.com').then(
-        function(response) {
-          return response.data;
-        }
-      );
-    }
-  }
-}]);
+// angular.module('myApp.factoryService', [])
+// .factory('factoryService', ['$http', function($http){
+//   return {
+//     getNews: function() {
+//       return $http.get('http://weather.com').then(
+//         function(response) {
+//           return response.data;
+//         }
+//       );
+//     }
+//   }
+// }]);
 
